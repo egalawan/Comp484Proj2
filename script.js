77a75cc4bc9810c5065117b00362de3d563b94a1
Thisura Nawalage <thisura@MacBook-Air-120.attlocal.net>
2023-11-08 03:43:38 -0800

    Adding Pic of 'Nami' Centering texts and adding Tuna Can when clicking on Tuna Can

--- script.js
+++ script.js
@@ -7,40 +7,99 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     $('.treat-button').click(clickedTreatButton);
     $('.play-button').click(clickedPlayButton);
     $('.exercise-button').click(clickedExerciseButton);
-  
-
-  
-    
+    $('.sleep-button').click(clickedSleepButton);
   })
   
+
     // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
-    var pet_info = {name:"My Pet Name", weight:"??", happiness:"??"};
+    var pet_info = {name:"Nami", weight:"10", happiness:"30"};
   
+    // 'Treat' Button
     function clickedTreatButton() {
-      // Increase pet happiness
-      // Increase pet weight
+      for (const key in pet_info){
+        // Increase pet happiness
+        if (key === 'happiness'){
+          pet_info[key] = parseInt(pet_info[key],10) + 4;
+        }
+        // Increase pet weight
+        if (key === 'weight') {
+          pet_info[key] = parseInt(pet_info[key],10) + 2;
+        }
+        var treatImage = document.getElementById('treat-image');
+        treatImage.src = 'https://cdn.glitch.global/74676c65-7f92-4fbe-99a8-1a22e12f0a4a/dots%202.PNG?v=1699441411487';
+        treatImage.classList.remove('hidden-treat');
+
+        var petImage = document.getElementById('pet-image');
+        // pet eating cat but need to change
+        petImage.src = 'https://cdn.glitch.global/74676c65-7f92-4fbe-99a8-1a22e12f0a4a/dots.PNG?v=1699420764570';
+
+        setTimeout(function () {
+          treatImage.classList.add('hidden-treat');
+        }, 500);
+            }
+
       checkAndUpdatePetInfoInHtml();
     }
     
+    // 'Play' Button
     function clickedPlayButton() {
-      // Increase pet happiness
-      // Decrease pet weight
+      for (const key in pet_info){
+        // Increase pet happiness
+        if (key === 'happiness'){
+          pet_info[key] = parseInt(pet_info[key],10) + 1;
+        }
+        // Decrease pet weight
+        if (key === 'weight') {
+          pet_info[key] = parseInt(pet_info[key],10) - 2;
+        }
+      }
       checkAndUpdatePetInfoInHtml();
     }
     
+    // 'Exercise' Button
     function clickedExerciseButton() {
-      // Decrease pet happiness
-      // Decrease pet weight
+      for (const key in pet_info){
+        // Decrease pet happiness
+        if (key === 'happiness'){
+          pet_info[key] = parseInt(pet_info[key],10) - 5;
+        }
+        // Decrease pet weight
+        if (key === 'weight') {
+          pet_info[key] = parseInt(pet_info[key],10) - 3;
+        }
+      }
       checkAndUpdatePetInfoInHtml();
     }
-  
+
+    // 'Sleep' Button
+    function clickedSleepButton() {
+      for (const key in pet_info){
+        //Increase pet happiness
+        if (key === 'happiness'){
+          pet_info[key] = parseInt(pet_info[key],10) + 3;
+        }
+      }
+      checkAndUpdatePetInfoInHtml();
+    }
+
+    // making sure not negative then final update 
     function checkAndUpdatePetInfoInHtml() {
       checkWeightAndHappinessBeforeUpdating();  
       updatePetInfoInHtml();
     }
     
+    // Make sure the weight and the happiness don't go to negative numbers
     function checkWeightAndHappinessBeforeUpdating() {
       // Add conditional so if weight is lower than zero.
+      for(const key in pet_info){
+        if(key == 'weight' || key == 'happiness'){
+            //if less than 0 keep at 0
+            if(parseInt(pet_info[key],10) < 0){ 
+                pet_info[key] = 0;
+                //add something to let the user know to feed pet or make pet happier
+            }
+        }
+      }
     }
     
     // Updates your HTML with the current values in your pet_info object
